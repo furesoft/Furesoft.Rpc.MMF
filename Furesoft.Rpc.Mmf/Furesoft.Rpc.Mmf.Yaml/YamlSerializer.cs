df1a86af8895bc2d475cf54208cdd5c1489a0911
Chris Anders <chris.anders@outlook.de>
2018-12-17 13:56:41 +0100

    added yaml serializer

---
+++ Furesoft.Rpc.Mmf/Furesoft.Rpc.Mmf.Yaml/YamlSerializer.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Furesoft.Rpc.Mmf.Yaml
+{
+    public class YamlSerializer : RpcSerializer
+    {
+        public override RpcMessage Deserialize(byte[] data)
+        {
+            var serializer = new SharpYaml.Serialization.Serializer();
+
+            return serializer.Deserialize<RpcMessage>(new MemoryStream(data));
+        }
+
+        public override byte[] Serialize(RpcMessage msg)
+        {
+            var serializer = new SharpYaml.Serialization.Serializer();
+            var text = serializer.Serialize(msg);
+
+            return Encoding.ASCII.GetBytes(text);
+        }
+    }
+}
